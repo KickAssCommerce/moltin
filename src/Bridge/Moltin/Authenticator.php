6f48e624edc64d3d1d3c11c78473ff4aa5448162
Sander Mangel <sander@sandermangel.nl>
2017-02-12 01:02:48 +0100

    moved bridge files from core to own module

---
+++ src/Bridge/Moltin/Authenticator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace KickAss\Moltin\Bridge\Moltin;
+
+use Moltin\SDK\Facade\Moltin as Moltin;
+
+class Authenticator implements \KickAss\Commerce\Application\AuthenticatorInterface
+{
+    /**
+     * @return bool
+     * @throws \KickAss\Commerce\Exception\FailedToAuthenticateException
+     */
+    public function authenticate()
+    {
+        // Authenticate credentials
+        $result = Moltin::Authenticate(
+            'ClientCredentials',
+            [
+                'client_id'     => getenv('MOLTIN_CLIENT_ID'),
+                'client_secret' => getenv('MOLTIN_CLIENT_SECRET')
+            ]
+        );
+
+        if ($result === false) {
+            throw new \KickAss\Commerce\Authentication\Exception\FailedToAuthenticateException();
+        }
+        return $result;
+    }
+}
